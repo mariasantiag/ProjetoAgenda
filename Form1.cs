16b04cb50c30bea28651c3f259e388c9c31567a5
Guilherme Mitsuyuki <andre.bido4406@gmail.com>
2024-10-08 10:37:57 -0300

    add: tela login e cadastro

--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,51 @@
 namespace ProjetoAgenda
 {
-    public partial class Form1 : Form
+    public partial class frmLogin : Form
     {
-        public Form1()
+        public frmLogin()
         {
             InitializeComponent();
         }
+
+        private void habilitarBotaoLogin()
+        {
+            // Se a caixa de usuário for diferente de vazio e a quantidade caracteres na senha for 8 ou maior, ele conseguirá logar
+            if (txtUsuario.Text != "" && txtSenha.Text.Length >= 8)
+            {
+                btnEntrar.Enabled = true;
+            }
+            else
+            {
+                btnEntrar.Enabled = false;
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            frmcadastro Cadastro = new frmcadastro();
+            Cadastro.Show();
+        }
+
+
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtUsuario_TextChanged(object sender, EventArgs e)
+        {
+            habilitarBotaoLogin();
+        }
+
+        private void txtSenha_TextChanged(object sender, EventArgs e)
+        {
+            habilitarBotaoLogin();
+        }
     }
-}
+} 
