52fd900dc8def0e4134e9503c0c3973b76706c95
Maria Fernanda <fernandamaria5501@gmail.com>
2024-10-29 14:55:25 -0300

    modificado: usuario controller

---
+++ Controller/UsuarioController.cs
@@ -0,0 +1,51 @@
+﻿using MySql.Data.MySqlClient;
+using ProjetoAgenda.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAgenda.Controller
+{
+    internal class UsuarioController
+    {
+        public bool AddUsuario (string nome, string usuario, string telefone, string senha)
+        {
+            // Cria a conexão, estou utilizando a classe ConexaoDB que está dentro da pasta DATA
+            MySqlConnection conexao = ConexaoDB.CriarConexao();
+
+            // Comando SQL que será executado
+            string sql = "INSERT INTO tbUsuarios (nome, usuario, telefone, senha) VALUES (@nome, @usuario, @telefone, @senha)";
+
+            // Abri a conexão com o banco
+            conexao.Open();
+
+            // Responsável por executar o comando SQL
+            MySqlCommand comando = new MySqlCommand(sql, conexao);
+
+            // Estou trocando o valor dos @ pelas informações que serão cadastradas
+            // Essas informações vieram dos parametros da função
+            comando.Parameters.AddWithValue("@nome", nome);
+            comando.Parameters.AddWithValue("@usuario", usuario);
+            comando.Parameters.AddWithValue("@telefone", telefone);
+            comando.Parameters.AddWithValue("@senha", senha);
+    
+            // Executando no banco de dados
+            int linhasAfetadas = comando.ExecuteNonQuery();
+
+            // Fechei a conexão
+            conexao.Close();
+
+            if (linhasAfetadas > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
--- Views/frmcadastro.cs
+++ Views/frmcadastro.cs
@@ -1,4 +1,5 @@
 ﻿using MySql.Data.MySqlClient;
+using ProjetoAgenda.Controller;
 using ProjetoAgenda.Data;
 using System;
 using System.Collections.Generic;
@@ -64,30 +65,27 @@ namespace ProjetoAgenda
 
         private void btnCadastrar_Click(object sender, EventArgs e)
         {
-            MySqlConnection conexao = ConexaoDB.CriarConexao();
+            // Pegando os dados do formulário
+            string nome = txtNome.Text; 
+            string usuario = txtUsuario.Text;
+            string telefone = txtTelefone.Text;
+            string senha = txtSenha.Text;  
 
-            // abrindo conexao
-            conexao.Open();
+            // Instanciando o objeto usando UsuarioController
+            UsuarioController controleUsuario = new UsuarioController();
 
-            // criando o comando SQL para inserir o usuário
-            string sql = $"INSERT INTO tbUsuarios (nome, usuario, telefone, senha) VALUES (@nome, @usuario, @telefone, @senha)";
-            
-            // criando o comando
-            MySqlCommand comando = new MySqlCommand(sql, conexao);
+            //Inserindo o usuário
+             bool resultado = controleUsuario.AddUsuario(nome, usuario, telefone, senha);
 
-            comando.Parameters.AddWithValue("@nome", txtNome.Text);
-            comando.Parameters.AddWithValue("@usuario", txtUsuario.Text);
-            comando.Parameters.AddWithValue("@telefone", txtTelefone.Text);
-            comando.Parameters.AddWithValue("@senha", txtSenha.Text);
-
-            // executando a instrução SQL no bancoV
-            comando.ExecuteNonQuery();
-
-            // fechando a conexão com o banco
-            conexao.Close();
-
-            MessageBox.Show("Cadastro efetuado com sucesso! \n Você já pode realizar o login");
-            this.Close();
+            if (resultado) 
+            {
+                MessageBox.Show("Cadastro efetuado com sucesso.");
+            }
+            else
+            {
+                MessageBox.Show("Não foi possível cadastrar o usuário.");
+            }
         }
+
     }
 }
